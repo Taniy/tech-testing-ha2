3d6efd63b0d3b700e4201084bc9ae49e0f13067c
tanya <tanya@tanya.com>
2014-10-27 05:51:08 +0400

    fixed paths

--- tests/helpers/config.py
+++ tests/helpers/config.py
@@ -10,5 +10,5 @@ CAMPAIGN = ''.join([random.choice(string.ascii_letters + string.digits) for n in
 REF = 'https://play.google.com/store/apps/details?id=com.rovio.retry'
 TEXT = 'qqqqqq'
 TITLE = 'eeeeee'
-SMALL_IMAGE  = '/home/sergey/tan/tech-testing-ha2/866.jpg'
-BIG_IMAGE  = '/home/sergey/tan/tech-testing-ha2/luli.jpg'
+SMALL_IMAGE  = '866.jpg'
+BIG_IMAGE  = 'luli.jpg'
